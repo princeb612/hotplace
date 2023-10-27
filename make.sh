6bb864279892cd3b338811d8fe2b4dc8b9a76f3f
princeb612 <princeb612.kr@gmail.com>
2023-10-27 14:14:24 +0900

    hotplace rev.372 build and test

--- make.sh
+++ make.sh
@@ -17,32 +17,13 @@ COMMENTS
 
 :<< HELP
     ctest - build and run ctest
+    debug - debug build
+    format - clang-format
+    pch - precompiled header
     redist - redistribute MSYS2(MINGW) binaries
     test - run examples
+    ex) ./make.sh format debug pch
 HELP
-
-do_clangformat=0
-do_ctest=0
-do_redist=0
-do_test=0
-
-CXXFLAGS=''
-args=("$@")
-if [ ${#args[@]} -ne 0 ]; then
-    for arg in ${args[@]}; do
-        if [ $arg = 'format' ]; then
-            do_clangformat=1
-        elif [ $arg = 'ctest' ]; then
-            do_ctest=1
-        elif [ $arg = 'redist' ]; then
-            do_redist=1
-        elif [ $arg = 'test' ]; then
-            do_test=1
-        elif [ $arg = 'debug' ]; then
-            CXXFLAGS='-DDEBUG -g'
-        fi
-    done
-fi
 
 :<< SWITCHES
     SUPPORT_SHARED  - recompile openssl, jansson with -fPIC
@@ -54,20 +35,45 @@ SWITCHES
 if [ $OSTYPE = "msys" ]; then
     export SUPPORT_SHARED=0
     export SUPPORT_ODBC=0
-    export SUPPORT_PCH=1
     true
 else
     export SUPPORT_SHARED=0
     export SUPPORT_ODBC=0
-    export SUPPORT_PCH=0
     export SET_STDCPP=c++11
     # -Wl,--copy-dt-needed-entries # DSO missing from command line
     CXXFLAGS='-Wl,--copy-dt-needed-entries '${CXXFLAGS}
     true
 fi
 
+do_clangformat=0
+do_ctest=0
+do_redist=0
+do_test=0
+
+CXXFLAGS=''
+SUPPORT_PCH=0
+args=("$@")
+if [ ${#args[@]} -ne 0 ]; then
+    for arg in ${args[@]}; do
+        if [ $arg = 'format' ]; then
+            do_clangformat=1
+        elif [ $arg = 'ctest' ]; then
+            do_ctest=1
+        elif [ $arg = 'redist' ]; then
+            do_redist=1
+        elif [ $arg = 'pch' ]; then
+            SUPPORT_PCH=1
+        elif [ $arg = 'test' ]; then
+            do_test=1
+        elif [ $arg = 'debug' ]; then
+            CXXFLAGS='-DDEBUG -g'
+        fi
+    done
+fi
+
 export HOTPLACE_HOME=$(pwd)
 export CXXFLAGS
+export SUPPORT_PCH
 
 # clang-format
 if [ $do_clangformat = 1 ]; then
@@ -83,7 +89,7 @@ time make
 
 # ctest
 if [ $do_ctest = 1 ]; then
-    cd build/test/
+    cd ${HOTPLACE_HOME}/build/test/
     ctest
 fi
 # redist mingw binaries
@@ -95,7 +101,6 @@ if [ $do_redist = 1 ]; then
 fi
 # run build/test/test.sh
 if [ $do_test = 1 ]; then
-    cd ${HOTPLACE_HOME}
-    cd build/test/
+    cd ${HOTPLACE_HOME}/build/test/
     ./test.sh
 fi
